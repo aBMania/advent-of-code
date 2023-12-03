27a8544637f953b1e0cb0ed005a16fe7fc662588
Sylvain Bossut <sylvain.bossut@exaion.com>
2023-12-03 18:39:34 +0100

    ✨ Enhance solve 2023 day 02

--- src/bin/2023-03/main.rs
+++ src/bin/2023-03/main.rs
@@ -1,99 +1,108 @@
 use std::collections::BTreeMap;
+use std::iter::once;
+use itertools::Itertools;
 
 use advent_of_code::{CustomGrid, input_to_grid, read_input, should_submit, submit, NeighborsDiagonalIterator};
-
-const NEIGHBORS: [(isize, isize); 8] = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
 
 const DAY: u8 = 3;
 const YEAR: u16 = 2023;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let grid = input_to_grid::<char>(input).unwrap();
+    let grid: CustomGrid<char> = input_to_grid::<char>(input).unwrap();
 
-    let mut is_block_counted = false;
-    let mut block_sum = 0u32;
-    let mut total_sum = 0u32;
-    let mut last_seen_row = 0;
+    let mut iterator = grid.indexed_iter().peekable();
+    let mut sum = 0;
 
-    for ((row, col), value) in grid.indexed_iter() {
-        if last_seen_row != row || !value.is_ascii_digit() {
-            if is_block_counted {
-                total_sum += block_sum;
-            }
-
-            block_sum = 0;
-            is_block_counted = false;
+    while let Some(((row, col), value)) = iterator.next() {
+        match *value {
+            _ if value.is_ascii_digit() => {}
+            _ => continue
         }
 
-        if !value.is_ascii_digit() {
-            last_seen_row = row;
-            continue;
-        }
+        let group: Vec<_> =
+            once(((row, col), value)).chain(
+                iterator
+                    .peeking_take_while(|(_, value)| value.is_ascii_digit())
+            )
+                .collect();
 
-        if !is_block_counted {
-            is_block_counted = grid.iter_diagonal_neighbors(row, col)
-                .any(|(_, &neighbor_value)| {
-                    neighbor_value != '.' && !neighbor_value.is_ascii_digit()
-                });
-        }
+        let symbol = group
+            .iter()
+            .filter_map(|&((row, col), _)| {
+                grid
+                    .iter_diagonal_neighbors(row, col)
+                    .filter(|(_, &neighbor_value)| neighbor_value != '.' && !neighbor_value.is_ascii_digit())
+                    .map(|(pos, _)| pos)
+                    .next()
+            })
+            .next();
 
-        if let Some(value_digit) = value.to_digit(10) {
-            block_sum = block_sum * 10 + value_digit
+        if let Some(_) = symbol {
+            let group_sum: u32 = group
+                .into_iter()
+                .map(|(_, value)| value)
+                .collect::<String>()
+                .parse()
+                .unwrap();
+
+            sum += group_sum;
         }
-        last_seen_row = row
     }
 
-    Some(total_sum)
+    Some(
+        sum)
 }
 
 
 pub fn part_two(input: &str) -> Option<u32> {
     let grid: CustomGrid<char> = input_to_grid::<char>(input).unwrap();
 
-    let mut gear_map: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new();
-    let mut block_sum = 0u32;
-    let mut last_seen_row = 0;
-    let mut current_gear: Option<(usize, usize)> = None;
+    let mut iterator = grid.indexed_iter().peekable();
+    let mut gears: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new();
 
-    for ((row, col), value) in grid.indexed_iter() {
-        if last_seen_row != row || !value.is_ascii_digit() {
-            if let Some(current_gear) = current_gear {
-                gear_map.entry(current_gear).or_insert(vec![]).push(block_sum);
-            }
-
-            current_gear = None;
-            block_sum = 0;
+    while let Some(((row, col), value)) = iterator.next() {
+        match *value {
+            _ if value.is_ascii_digit() => {}
+            _ => continue
         }
 
-        if !value.is_ascii_digit() {
-            last_seen_row = row;
-            continue;
+        let group: Vec<_> =
+            once(((row, col), value)).chain(
+                iterator
+                    .peeking_take_while(|(_, value)| value.is_ascii_digit())
+            )
+                .collect();
+
+        let gear = group
+            .iter()
+            .filter_map(|&((row, col), _)| {
+                grid
+                    .iter_diagonal_neighbors(row, col)
+                    .filter(|(_, &neighbor_value)| neighbor_value == '*')
+                    .map(|(pos, _)| pos)
+                    .next()
+            })
+            .next();
+
+        if let Some(gear) = gear {
+            let group_sum: u32 = group
+                .into_iter()
+                .map(|(_, value)| value)
+                .collect::<String>()
+                .parse()
+                .unwrap();
+
+            gears.entry(gear).or_insert(vec![]).push(group_sum);
         }
-
-
-        if current_gear.is_none() {
-            current_gear = grid
-                .iter_diagonal_neighbors(row, col)
-                .filter(|(_, &neighbor_value)| neighbor_value == '*')
-                .map(|(pos, _)| pos)
-                .next()
-        }
-
-
-        if let Some(value_digit) = value.to_digit(10) {
-            block_sum = block_sum * 10 + value_digit
-        }
-        last_seen_row = row
     }
 
     Some(
-        gear_map
-            .into_iter()
-            .map(|(_, blocks)| {
-                if blocks.len() != 2 {
-                    0
-                } else {
-                    blocks.get(0).unwrap() * blocks.get(1).unwrap()
+        gears
+            .iter()
+            .filter_map(|(_, sums)| {
+                match sums.len() {
+                    2 => Some(sums.get(0).unwrap() * sums.get(1).unwrap()),
+                    _ => None
                 }
             })
             .sum()
@@ -159,14 +168,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let example = read_example(DAY, YEAR);
-        // TODO: set example expected response
         assert_eq!(part_one(&example), Some(4361));
     }
 
     #[test]
     fn test_part_two() {
         let example = read_example(DAY, YEAR);
-        // TODO: set example expected response
         assert_eq!(part_two(&example), Some(467835));
     }
 }
